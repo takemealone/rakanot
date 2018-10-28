9a4aecf51d6420091063a995c2a928ac2020422e
takemealone <44326118+takemealone@users.noreply.github.com>
2018-10-28 21:33:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,23 +31,10 @@ client.on('ready', () => {
   console.log('')
 });
 
-client.on('message', message => {//for dev
+client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
-
-if (message.content.startsWith(adminprefix + 'setgawdwdwme')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else
-  if (message.content.startsWith(adminprefix + 'setwdwdname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'setawdwdwvatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else
+  
 if (message.content.startsWith(adminprefix + 'stream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/faresgameryt");
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
