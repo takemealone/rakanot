312ca652c71c6e080cbe472987ee042726ec9591
takemealone <44326118+takemealone@users.noreply.github.com>
2018-10-28 21:31:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const devs = ['460606140666085378'];
+const adminprefix = "-!";
+
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -27,29 +31,25 @@ client.on('ready', () => {
   console.log('')
 });
 
-
-
-const adminprefix = "-!";
-const devs = ['280749272498962432' , '324249224969584642' , '431474404007084035'];
-Rocket.on('message', message => {//for dev
+client.on('message', message => {//for dev
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
 
 if (message.content.startsWith(adminprefix + 'setgawdwdwme')) {
-  Rocket.user.setGame(argresult);
+  client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else
   if (message.content.startsWith(adminprefix + 'setwdwdname')) {
-Rocket.user.setUsername(argresult).then
+client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
   if (message.content.startsWith(adminprefix + 'setawdwdwvatar')) {
-Rocket.user.setAvatar(argresult);
+client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else
 if (message.content.startsWith(adminprefix + 'stream')) {
-  Rocket.user.setGame(argresult, "https://www.twitch.tv/faresgameryt");
+  client.user.setGame(argresult, "https://www.twitch.tv/faresgameryt");
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
 
